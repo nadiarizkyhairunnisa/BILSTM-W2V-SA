3d330007233f520d44e7028d78330b17f1dddc7b
nadiarizkyhairunnisa <09021281823039@student.unsri.ac.id>
2023-03-30 23:45:12 +0700

    feat: 🎸 add a new function to create evaluation blueprint table

--- Laravel/database/migrations/MigrationHelper.php
+++ Laravel/database/migrations/MigrationHelper.php
@@ -11,4 +11,18 @@ class MigrationHelper
         $table->text('Processed Review');
         $table->enum('Label', ['1', '0']);
     }
+
+    public static function createModelEvaluationTable(Blueprint $table)
+    {
+        $table->id('Epoch');
+        $table->double('Loss');
+        $table->double('Accuracy');
+        $table->double('F1 Score');
+        $table->double('Precision');
+        $table->double('Recall');
+        $table->double('TP');
+        $table->double('TN');
+        $table->double('FP');
+        $table->double('FN');
+    }
 }
